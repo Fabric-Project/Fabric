57d662a1cf09541422354fd0a137f0908bbad9bb
vade <vade@vade.info>
2025-10-16 16:38:44 -0400

    Fix serialization

--- Fabric/Nodes/Utility/PixelsToUnitsNode.swift
+++ Fabric/Nodes/Utility/PixelsToUnitsNode.swift
@@ -16,9 +16,43 @@ public class PixelsToUnitsNode : Node
     public override class var nodeType:Node.NodeType { Node.NodeType.Utility }
     
     // Ports
-    public let inputCursorPosition:NodePort<simd_float2> = NodePort<simd_float2>(name: "Position" , kind: .Inlet)
-    public let outputUnitPosition:NodePort<simd_float3> = NodePort<simd_float3>(name: "Units" , kind: .Outlet)
+    public let inputCursorPosition:NodePort<simd_float2>
+    public let outputUnitPosition:NodePort<simd_float3>
     public override var ports: [AnyPort] { [ self.inputCursorPosition, self.outputUnitPosition] + super.ports}
+    
+    public required init(context: Context)
+    {
+        self.inputCursorPosition = NodePort<simd_float2>(name: "Position" , kind: .Inlet)
+        self.outputUnitPosition = NodePort<simd_float3>(name: "Units" , kind: .Outlet)
+
+        super.init(context: context)
+    }
+    
+    enum CodingKeys : String, CodingKey
+    {
+        case inputCursorPositionPort
+        case outputUnitPositionPort
+    }
+    
+    public override func encode(to encoder:Encoder) throws
+    {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.inputCursorPosition, forKey: .inputCursorPositionPort)
+        try container.encode(self.outputUnitPosition, forKey: .outputUnitPositionPort)
+
+        try super.encode(to: encoder)
+    }
+    
+    public required init(from decoder: any Decoder) throws
+    {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.inputCursorPosition = try container.decode(NodePort<simd_float2>.self, forKey: .inputCursorPositionPort)
+        self.outputUnitPosition = try container.decode(NodePort<simd_float3>.self, forKey: .outputUnitPositionPort)
+
+        try super.init(from: decoder)
+    }
     
     public override func execute(context:GraphExecutionContext,
                                  renderPassDescriptor: MTLRenderPassDescriptor,
