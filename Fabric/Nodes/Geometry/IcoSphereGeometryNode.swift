43c8cff7cbb1b70013eb4bab593f2197c57e31f8
vade <vade@vade.info>
2025-10-24 15:59:21 -0400

    Geo

--- Fabric/Nodes/Geometry/IcoSphereGeometryNode.swift
+++ Fabric/Nodes/Geometry/IcoSphereGeometryNode.swift
@@ -13,62 +13,38 @@ public class IcoSphereGeometryNode : BaseGeometryNode
 {
     public override class var name:String { "IcoSphere Geometry" }
 
-    // Params
-    public let inputRadius:FloatParameter
-    public let inputResolutionParam:IntParameter
-    override public var inputParameters: [any Parameter] { [inputRadius, inputResolutionParam] + super.inputParameters}
+    override public class func registerPorts(context: Context) -> [(name: String, port: Port)] {
+        let ports = super.registerPorts(context: context)
+        
+        return  [
+        ("inputRadius",  ParameterPort(parameter:FloatParameter("inputRadius", 1.0, .inputfield))),
+        ("inputResolution",  ParameterPort(parameter:IntParameter("inputResolution", 1, .inputfield))),
+
+        ] + ports
+    }
+    
+    // Proxy Port
+    public var inputRadius:ParameterPort<Float> { port(named: "inputRadius")  }
+    public var inputResolution:ParameterPort<Int> { port(named: "inputResolution")  }
 
     public override var geometry: IcoSphereGeometry { _geometry }
     private let _geometry = IcoSphereGeometry(radius: 1.0, resolution: 1)
-
-    required public init(context: Context)
-    {
-        self.inputRadius = FloatParameter("Radius", 1.0, .inputfield)
-        self.inputResolutionParam = IntParameter("Resolution", 1, .inputfield)
-
-
-        super.init(context: context)
-    }
-        
-    enum CodingKeys : String, CodingKey
-    {
-        case inputRadiusParameter
-        case inputResolutionParameter
-    }
-    
-    override public func encode(to encoder:Encoder) throws
-    {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(self.inputRadius, forKey: .inputRadiusParameter)
-        try container.encode(self.inputResolutionParam, forKey: .inputResolutionParameter)
-        
-        try super.encode(to: encoder)
-    }
-    
-    required public init(from decoder: any Decoder) throws
-    {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.inputRadius = try container.decode(FloatParameter.self, forKey: .inputRadiusParameter)
-        self.inputResolutionParam = try container.decode(IntParameter.self, forKey: .inputResolutionParameter)
-        
-        try super.init(from: decoder)
-    }
     
     override public func evaluate(geometry: Geometry, atTime: TimeInterval) -> Bool
     {
         var shouldOutputGeometry = super.evaluate(geometry: geometry, atTime: atTime)
 
-        if self.inputRadius.valueDidChange
+        if self.inputRadius.valueDidChange,
+           let inputRadius = self.inputRadius.value
         {
-            self.geometry.radius = self.inputRadius.value
+            self.geometry.radius = inputRadius
             shouldOutputGeometry = true
         }
                 
-        if  self.inputResolutionParam.valueDidChange
+        if  self.inputResolution.valueDidChange,
+            let inputResolution = self.inputResolution.value
         {
-            self.geometry.resolution =  self.inputResolutionParam.value
+            self.geometry.resolution = inputResolution
             shouldOutputGeometry = true
         }
        
