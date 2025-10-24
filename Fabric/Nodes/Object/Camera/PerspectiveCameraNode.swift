a3eac7f79086ffedacca051b4f7f73f521da351f
vade <vade@vade.info>
2025-10-24 12:24:02 -0400

    Camera

--- Fabric/Nodes/Object/Camera/PerspectiveCameraNode.swift
+++ Fabric/Nodes/Object/Camera/PerspectiveCameraNode.swift
@@ -12,17 +12,24 @@ import Metal
 
 public class PerspectiveCameraNode : ObjectNode<PerspectiveCamera>
 {
-    public override class var name:String { "Perspective Camera" }
-    public override class var nodeType:Node.NodeType { Node.NodeType.Object(objectType: .Camera) }
-
-    // Params
-    public var inputLookAt:Float3Parameter
-    public override var inputParameters: [any Parameter] {  [inputLookAt] + super.inputParameters}
+    override public class var name:String { "Perspective Camera" }
+    override public class var nodeType:Node.NodeType { Node.NodeType.Object(objectType: .Camera) }
+    override public class var nodeExecutionMode: Node.ExecutionMode { .Consumer }
+    override public class var nodeTimeMode: Node.TimeMode { .None }
+    override public class var nodeDescription: String { "Provides Perspective Camera for the Scene."}
 
     // Ports
-//    public let outputCamera:NodePort<Camera>
-//    public override var ports: [Port] { [outputCamera] + super.ports }
-
+    override public class func registerPorts(context: Context) -> [(name: String, port: Port)] {
+        let ports = super.registerPorts(context: context)
+        
+        return  [
+                    ("inputLookAt", ParameterPort(parameter:Float3Parameter("Look At", simd_float3(repeating:0), .inputfield )) ),
+                ] + ports
+    }
+    
+    // Proxy Port
+    public var inputLookAt:ParameterPort<simd_float3> { port(named: "inputLookAt") }
+    
     override public var object: PerspectiveCamera?
     {
         camera
@@ -30,63 +37,29 @@ public class PerspectiveCameraNode : ObjectNode<PerspectiveCamera>
     
     private let camera = PerspectiveCamera(position: .init(repeating: 5.0), near: 0.01, far: 500.0, fov: 30)
 
-    
-    public required init(context:Context)
+    override public func startExecution(context:GraphExecutionContext)
     {
-        self.inputLookAt = Float3Parameter("Look At", simd_float3(repeating:0), .inputfield )
-//        self.outputCamera = NodePort<Camera>(name: PerspectiveCameraNode.name, kind: .Outlet)
-                
-        super.init(context: context)
+        super.startExecution(context: context)
         
         self.inputPosition.value = .init(repeating: 5.0)
         
-
         self.camera.lookAt(target: simd_float3(repeating: 0))
         self.camera.position = self.inputPosition.value ?? .zero
         self.camera.scale = self.inputScale.value ?? .zero
-
+        
         let orientation = self.inputOrientation.value ?? .zero
         self.camera.orientation = simd_quatf(angle: orientation.w,
-                                        axis: simd_float3(x: orientation.x,
-                                                          y: orientation.y,
-                                                          z: orientation.z) )
-        
+                                             axis: simd_float3(x: orientation.x,
+                                                               y: orientation.y,
+                                                               z: orientation.z) )
     }
     
-    enum CodingKeys : String, CodingKey
-    {
-        case inputLookAtParameter
-//        case outputCameraPort
-    }
-    
-    public override func encode(to encoder:Encoder) throws
-    {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(self.inputLookAt, forKey: .inputLookAtParameter)
-//        try container.encode(self.outputCamera, forKey: .outputCameraPort)
-        
-        try super.encode(to: encoder)
-    }
-    
-    public required init(from decoder: any Decoder) throws
-    {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.inputLookAt = try container.decode(Float3Parameter.self, forKey: .inputLookAtParameter)
-//        self.outputCamera = try container.decode(NodePort<Camera>.self, forKey: .outputCameraPort)
-        
-        try super.init(from: decoder)
-        
-        self.camera.lookAt(target: self.inputLookAt.value)
-    }
-
     override public func evaluate(object: Object?, atTime: TimeInterval) -> Bool
     {
         let shouldUpdate = super.evaluate(object: object, atTime: atTime)
 
         // This needs to fire every frame
-        self.camera.lookAt(target: self.inputLookAt.value)
+        self.camera.lookAt(target: self.inputLookAt.value ?? .zero)
         
         return shouldUpdate
     }
@@ -96,16 +69,13 @@ public class PerspectiveCameraNode : ObjectNode<PerspectiveCamera>
                                  commandBuffer: MTLCommandBuffer)
     {
         let shouldUpdate = self.evaluate(object: self.camera, atTime: context.timing.time)
-        
-//        if shouldUpdate
-//        {
-//            self.outputCamera.send(self.camera)
-//        }
     }
     
     public override func resize(size: (width: Float, height: Float), scaleFactor: Float)
     {
         self.camera.aspect = size.width / size.height
     }
+    
+  
 }
 
