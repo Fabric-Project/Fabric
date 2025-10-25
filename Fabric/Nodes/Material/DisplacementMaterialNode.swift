46160cbca78de4c0f90241b61ae83e40e3ea7999
vade <vade@vade.info>
2025-10-25 10:27:30 -0400

    Displace Material

--- Fabric/Nodes/Material/DisplacementMaterialNode.swift
+++ Fabric/Nodes/Material/DisplacementMaterialNode.swift
@@ -15,31 +15,36 @@ public class DisplacementMaterialNode: BaseMaterialNode
     public class DisplacementMaterial : SourceMaterial { }
     
     public override class var name:String {  "Displacement Material" }
+    override public class var nodeDescription: String { "Displace Geometry using an Images luminance or rgb values."}
 
-    // Params
-    public let inputAmount:FloatParameter
-    public let inputLumaVsRGBAmount:FloatParameter
-    public let inputMinPointSize:FloatParameter
-    public let inputMaxPointSize:FloatParameter
-    public let inputBrightness:FloatParameter
-    public override var inputParameters: [any Parameter] {
-        [
-        self.inputAmount,
-        self.inputLumaVsRGBAmount,
-        self.inputMinPointSize,
-        self.inputMaxPointSize,
-        self.inputBrightness] + super.inputParameters
-    }
-    
     // Ports
-    public let inputTexture:NodePort<EquatableTexture>
-    public let inputDisplacementTexture:NodePort<EquatableTexture>
-    public let inputPointSpriteTexture:NodePort<EquatableTexture>
-    public override var ports: [Port] {   [
-        self.inputTexture,
-        self.inputDisplacementTexture,
-        self.inputPointSpriteTexture] + super.ports
+    override public class func registerPorts(context: Context) -> [(name: String, port: Port)] {
+        let ports = super.registerPorts(context: context)
+        
+        return [
+            ("inputTexture", NodePort<EquatableTexture>(name: "Image", kind: .Inlet)),
+            ("inputDisplacementTexture", NodePort<EquatableTexture>(name: "Displacement Image", kind: .Inlet)),
+            ("inputPointSpriteTexture", NodePort<EquatableTexture>(name: "Point Sprite Image", kind: .Inlet)),
+            ("inputAmount", ParameterPort(parameter:FloatParameter("Amount", 0.0, 0.0, 2.0, .slider))),
+            ("inputLumaVsRGBAmount", ParameterPort(parameter:FloatParameter("Luma v RGB", 0.0, 0.0, 1.0, .slider))),
+            ("inputMinPointSize", ParameterPort(parameter:FloatParameter("Min PointSize", 1.0, 0.5, 128.0, .slider))),
+            ("inputMaxPointSize", ParameterPort(parameter:FloatParameter("Max PointSize", 1.0, 0.5, 128.0, .slider))),
+            ("inputBrightness", ParameterPort(parameter:FloatParameter("Brightness", 1.0, 0.0, 2.0, .slider))),
+        ] + ports
     }
+
+    public var inputFilePathParam:ParameterPort<String>  { port(named: "inputFilePathParam") }
+    public var outputTexturePort:NodePort<EquatableTexture> { port(named: "outputTexturePort") }
+
+    // Proxy Params
+    public var inputTexture:NodePort<EquatableTexture> { port(named: "inputTexture") }
+    public var inputDisplacementTexture:NodePort<EquatableTexture> { port(named: "inputDisplacementTexture") }
+    public var inputPointSpriteTexture:NodePort<EquatableTexture> { port(named: "inputPointSpriteTexture") }
+    public var inputAmount:ParameterPort<Float> { port(named: "inputAmount") }
+    public var inputLumaVsRGBAmount:ParameterPort<Float> { port(named: "inputLumaVsRGBAmount") }
+    public var inputMinPointSize:ParameterPort<Float> { port(named: "inputMinPointSize") }
+    public var inputMaxPointSize:ParameterPort<Float> { port(named: "inputMaxPointSize") }
+    public var inputBrightness:ParameterPort<Float> { port(named: "inputBrightness") }
     
     
     public override var material: DisplacementMaterial {
@@ -50,91 +55,27 @@ public class DisplacementMaterialNode: BaseMaterialNode
     
 //    private var depthStencilDescriptor:MTLDepthStencilDescriptor
         
+   
     required public init(context: Context)
     {
         let bundle = Bundle(for: Self.self)
         let shaderURL = bundle.url(forResource: "DisplacementMaterial", withExtension: "metal", subdirectory: "Materials")
         
         self._material = DisplacementMaterial(pipelineURL: shaderURL!)
-
-        self.inputAmount = FloatParameter("Amount", 0.0, 0.0, 2.0, .slider)
-        self.inputLumaVsRGBAmount = FloatParameter("Luma v RGB", 0.0, 0.0, 1.0, .slider)
-        self.inputMinPointSize = FloatParameter("Min PointSize", 1.0, 0.5, 128.0, .slider)
-        self.inputMaxPointSize = FloatParameter("Max PointSize", 1.0, 0.5, 128.0, .slider)
-        self.inputBrightness = FloatParameter("Brightness", 1.0, 0.0, 2.0, .slider)
-
-        self.inputTexture = NodePort<EquatableTexture>(name: "Texture", kind: .Inlet)
-        self.inputDisplacementTexture = NodePort<EquatableTexture>(name: "Displacement Texture", kind: .Inlet)
-        self.inputPointSpriteTexture = NodePort<EquatableTexture>(name: "Point Sprite Texture", kind: .Inlet)
-
-//        self.depthStencilDescriptor = Self.setupDepthStencil()
-
-        super.init(context: context)
-
-        self.material.setup()
-
-//        self.material.depthStencilState = context.device.makeDepthStencilState(descriptor: self.depthStencilDescriptor)
-//        self.material.onBind = { renderEncoder in
-//            renderEncoder.setStencilReferenceValue(99)
-//        }
         
-    }
-    
-    enum CodingKeys : String, CodingKey
-    {
-        case inputTexturePort
-        case inputDisplacementTexturePort
-        case inputPointSpriteTexturePort
-        case inputAmountParameter
-        case inputLumaVRGBAmountParameter
-        case inputMinPointSizeParameter
-        case inputMaxPointSizeParameter
-        case inputBrightnessParameter
+        super.init(context: context)
     }
     
     public required init(from decoder: any Decoder) throws
     {
-        guard let decodeContext = decoder.context else
-        {
-            fatalError("Required Decode Context Not set")
-        }
-        
         let bundle = Bundle(for: Self.self)
         let shaderURL = bundle.url(forResource: "DisplacementMaterial", withExtension: "metal", subdirectory: "Materials")
         
         self._material = DisplacementMaterial(pipelineURL: shaderURL!)
         
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-
-        self.inputAmount = try container.decode(FloatParameter.self, forKey: .inputAmountParameter)
-        self.inputLumaVsRGBAmount = try container.decode(FloatParameter.self, forKey: .inputLumaVRGBAmountParameter)
-        self.inputMinPointSize = try container.decode(FloatParameter.self, forKey: .inputMinPointSizeParameter)
-        self.inputMaxPointSize = try container.decode(FloatParameter.self, forKey: .inputMaxPointSizeParameter)
-        self.inputBrightness = try container.decode(FloatParameter.self, forKey: .inputBrightnessParameter)
-
-        self.inputTexture = try container.decode(NodePort<EquatableTexture>.self, forKey: .inputTexturePort)
-        self.inputDisplacementTexture = try container.decode(NodePort<EquatableTexture>.self, forKey: .inputDisplacementTexturePort)
-        self.inputPointSpriteTexture = try container.decode(NodePort<EquatableTexture>.self, forKey: .inputPointSpriteTexturePort)
-
-//        self.depthStencilDescriptor = Self.setupDepthStencil()
-
         try super.init(from: decoder)
-
-        self.material.context = decodeContext.documentContext
-//        self.material.setup()
-
-//        self.material.blending = .additive
-//        self.material.rgbBlendOperation = .add
-//        self.material.depthWriteEnabled = false
-//        self.material.depthCompareFunction = .always
-//        self.material.blending
-//        
-//        self.material.onBind = { renderEncoder in
-//            renderEncoder.setStencilReferenceValue(127)
-//        }
-        
     }
-    
+        
     class private func setupDepthStencil() -> MTLDepthStencilDescriptor
     {
         let stencil = MTLStencilDescriptor()
@@ -170,22 +111,7 @@ public class DisplacementMaterialNode: BaseMaterialNode
 
 //        return depthStencil
     }
-    
-    public override func encode(to encoder:Encoder) throws
-    {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(self.inputAmount, forKey: .inputAmountParameter)
-        try container.encode(self.inputLumaVsRGBAmount, forKey: .inputLumaVRGBAmountParameter)
-        try container.encode(self.inputMinPointSize, forKey: .inputMinPointSizeParameter)
-        try container.encode(self.inputMaxPointSize, forKey: .inputMaxPointSizeParameter)
-        try container.encode(self.inputBrightness, forKey: .inputBrightnessParameter)
-        try container.encode(self.inputTexture, forKey: .inputTexturePort)
-        try container.encode(self.inputDisplacementTexture, forKey: .inputDisplacementTexturePort)
-        try container.encode(self.inputPointSpriteTexture, forKey: .inputPointSpriteTexturePort)
-
-        try super.encode(to: encoder)
-    }
+   
     
     override public func evaluate(material: Material, atTime: TimeInterval) -> Bool
     {
@@ -218,33 +144,38 @@ public class DisplacementMaterialNode: BaseMaterialNode
             }
         }
         
-        if self.inputAmount.valueDidChange
+        if self.inputAmount.valueDidChange,
+           let inputAmount = self.inputAmount.value
         {
-            self.material.set("amount", self.inputAmount.value)
+            self.material.set("amount", inputAmount)
             shouldOutput = true
         }
         
-        if self.inputLumaVsRGBAmount.valueDidChange
+        if self.inputLumaVsRGBAmount.valueDidChange,
+            let inputLumaVsRGBAmount = self.inputLumaVsRGBAmount.value
         {
-            self.material.set("lumaVPosMix", self.inputLumaVsRGBAmount.value)
+            self.material.set("lumaVPosMix", inputLumaVsRGBAmount)
             shouldOutput = true
         }
         
-        if self.inputMinPointSize.valueDidChange
+        if self.inputMinPointSize.valueDidChange,
+           let inputMinPointSize = self.inputMinPointSize.value
         {
-            self.material.set("minPointSize", self.inputMinPointSize.value)
+            self.material.set("minPointSize", inputMinPointSize)
             shouldOutput = true
         }
         
-        if  self.inputMaxPointSize.valueDidChange
+        if  self.inputMaxPointSize.valueDidChange,
+            let inputMaxPointSize = self.inputMaxPointSize.value
         {
-            self.material.set("maxPointSize", self.inputMaxPointSize.value)
+            self.material.set("maxPointSize", inputMaxPointSize)
             shouldOutput = true
         }
         
-        if self.inputBrightness.valueDidChange
+        if self.inputBrightness.valueDidChange,
+           let inputBrightness = self.inputBrightness.value
         {
-            self.material.set("brightness", self.inputBrightness.value)
+            self.material.set("brightness", inputBrightness)
             shouldOutput = true
         }
         
