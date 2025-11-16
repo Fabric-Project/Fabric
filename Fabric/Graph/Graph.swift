6dd9b55ceccf085b47103d56585bcc69a40fe255
Seyed Mojtaba Hosseini Zeidabadi <chenzook+github@gmail.com>
2025-11-16 13:14:52 +0330

    fix: typo in a printed "reference"

--- Fabric/Graph/Graph.swift
+++ Fabric/Graph/Graph.swift
@@ -455,7 +455,7 @@ internal import AnyCodable
             
             if let closestDistanceDirectionNodeTuples = relevantDistanceDirectionNodeTuples.sorted(by: { $0.Distance < $1.Distance }).first
             {
-                print("referece node", referenceNode.name)
+                print("reference node", referenceNode.name)
 
                 self.selectNode(node: closestDistanceDirectionNodeTuples.Node, expandSelection: expandSelection)
             }
