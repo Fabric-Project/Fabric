5ea34e9c8eb8ef20bcd2db2256bee924045806a2
vade <vade@vade.info>
2025-12-29 18:01:42 -0500

    Add markdown formatting to Fabric Graph Notes

--- Fabric/Views/NoteView.swift
+++ Fabric/Views/NoteView.swift
@@ -7,36 +7,66 @@
 
 import Foundation
 import SwiftUI
+import Textual
+
+private struct EditorView : View
+{
+    @Binding var string:String
+    @Binding var locked:Bool
+    
+    var body: some View
+    {
+        Group
+        {
+            if self.locked
+            {
+                StructuredText(markdown:self.string)
+                    .foregroundStyle( .primary )
+            }
+            else
+            {
+                TextEditor( text: self.$string)
+                    .textEditorStyle( .plain )
+                    .foregroundStyle( .primary )
+                    .font(Font.system(size: 10).monospaced() )
+                    .focusable(true, interactions: .edit)
+            }
+        }
+    }
+}
 
 struct NoteView : View
 {
     @Bindable var note:Note
     
+    @State var locked:Bool = true
+    
     var body: some View
     {
-        TextEditor( text: self.$note.note)
-            .textEditorStyle( .plain )
-        //        Text( LocalizedStringKey(self.note.note) )
-            .focusable(true)
-            .font(Font.system(size: 10).monospaced() )
+        EditorView( string: self.$note.note, locked: self.$locked)
             .padding()
             .frame(width: self.note.rect.size.width, height: self.note.rect.size.height, alignment: .topLeading)
             .background( Color.black.opacity(0.5) )
-            .foregroundStyle( .primary )
             .overlay(
+                
                 ZStack(alignment: .bottomTrailing)
                 {
                     RoundedRectangle(cornerRadius: 15)
-                        .stroke( Color.gray, lineWidth: 1 )
-//                        .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .miter, miterLimit: 0, dash: [5, 25], dashPhase: 0) )
-//                        .foregroundStyle( Color.gray )
+                        .stroke( Color.gray.opacity(0.5), lineWidth: 1 )
                         .frame(width: self.note.rect.size.width, height: self.note.rect.size.height, alignment: .topLeading)
                     
+                    Button("", systemImage: self.locked ? "lock.fill" : "lock.open.fill") {
+                        self.locked.toggle( )
+                    }
+                    .frame(width: 20, height: 20)
+                    .buttonStyle(.borderless)
+                    .offset(y: -self.note.rect.size.height + 20)
+                    
                     Rectangle()
-                        .fill( Color.gray.opacity(0.5))
-                        .stroke( Color.gray.opacity(0.5), lineWidth: 1 )
-                        .frame(width: 15, height:15)
-                        .gesture(DragGesture()
+                        .fill( Color.gray.opacity(0.25))
+                        .stroke( Color.gray.opacity(0.25), lineWidth: 1 )
+                        .frame(width: 25, height:25)
+                        .gesture(self.locked ? nil : DragGesture()
                             .onChanged { value in
                                 
                                 self.note.rect.size.width += value.translation.width
@@ -44,27 +74,20 @@ struct NoteView : View
                                 
                                 self.note.rect.size.width = max(120, self.note.rect.size.width)
                                 self.note.rect.size.height = max(60, self.note.rect.size.height)
-                                
-                                print("Resize ")
-                            }
-                            .onEnded { value in
-                                print("Drag Stopped")
                             }
                         )
+                        .opacity(self.locked ? 0 : 1)
                 }
             )
             .clipShape( RoundedRectangle(cornerRadius: 15) )
-            .gesture(DragGesture()
+            .gesture(self.locked ? nil : DragGesture()
                 .onChanged { value in
+                                        
                     self.note.rect.origin.x += value.translation.width
                     self.note.rect.origin.y += value.translation.height
-                    
-                    print("Reposition")
                 }
-                .onEnded { value in
-                    print("Reposition Stopped")
-                }
+               
             )
-            
+
     }
 }
