8406102a3e9e967169566f139254d4dd7c93e6f1
vade <vade@vade.info>
2026-05-12 01:55:20 -0400

    dd spotlight

--- Fabric/Nodes/NodeRegistry.swift
+++ Fabric/Nodes/NodeRegistry.swift
@@ -75,6 +75,7 @@ public class NodeRegistry {
     private var lightNodeClasses: [Node.Type] = [
         DirectionalLightNode.self,
         PointLightNode.self,
+        SpotLightNode.self,
     ]
     
     private var objectNodeClasses: [Node.Type] = [
