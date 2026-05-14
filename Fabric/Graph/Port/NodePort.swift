c03732a3d6a6db3f26637a353579441ade2a93e8
vade <vade@vade.info>
2026-05-14 19:00:06 -0400

    Add specialization to port, and additionally add isNan isInf checks - fix for #241

--- Fabric/Graph/Port/NodePort.swift
+++ Fabric/Graph/Port/NodePort.swift
@@ -10,6 +10,8 @@ import SwiftUI
 import Satin
 
 // Specialized port which facilitates sending a concrete type supported by Fabric .
+
+
 public class NodePort<Value : PortValueRepresentable>: Port
 {
     // BARF
@@ -310,12 +312,10 @@ public class NodePort<Value : PortValueRepresentable>: Port
     
     private func send(_ v:Value?, to other: NodePort<Value>, force:Bool = false)
     {
-        if other.value != v || force
-        {
-            other.value = v
-        }
+        // Specialized version
+       _send(v, to: other, force: force)
     }
-    
+
     private func send(_ v:Bool?, to other: NodePort<Bool>, force:Bool = false)
     {
         if other.value != v || force
@@ -334,7 +334,11 @@ public class NodePort<Value : PortValueRepresentable>: Port
     
     private func send(_ v:Float?, to other: NodePort<Float>, force:Bool = false)
     {
-        if other.value != v || force
+        let isNotNan = !(v?.isNaN ?? false)
+        let isNotInf = !(v?.isInfinite ?? false)
+
+        let safe = other.value != v && isNotInf && isNotNan
+        if  safe || force
         {
             other.value = v
         }
@@ -399,5 +403,28 @@ public class NodePort<Value : PortValueRepresentable>: Port
     private static func calcDirection(forType: Any.Type ) -> PortDirection
     {
         return .Horizontal
+    }
+}
+
+
+extension NodePort
+{
+    @_specialize(exported: true, where Value == Bool)
+    @_specialize(exported: true, where Value == Int)
+    @_specialize(exported: true, where Value == Float)
+    @_specialize(exported: true, where Value == String)
+    @_specialize(exported: true, where Value == matrix_float4x4)
+    @_specialize(exported: true, where Value == simd_float3)
+    @_specialize(exported: true, where Value == simd_float3)
+    @_specialize(exported: true, where Value == simd_float4)
+    @_specialize(exported: true, where Value == simd_quatf)
+    @usableFromInline
+    func _send(_ v: Value?, to other: NodePort<Value>, force: Bool = false) {
+        let v = Value.normalizePortValueForSend(v)
+
+        if other.value != v || force
+        {
+            other.value = v
+        }
     }
 }
